41af69fa0ead340bf45e58dbb97ed6b639a2ca68
Kotagiri <Hareesh.Kotagiri@pepsico.com>
2022-12-22 18:00:49 +0530

    updated js

--- shared.js
+++ shared.js
@@ -1,11 +1,17 @@
-window.getFullName = (fname, lname) => {
-    return `${fname} ${lname}`;
+function getFullName(fname, lname) {
+    let name = `${fname} ${lname}`;
+    sendToWeb(name);
 }
 
-window.calculateVals = (val1, val2) => {
-    return val1+val2;
+function calculateVals(val1, val2) {
+    let val = val1+val2;
+    sendToWeb(val);
 }
 
-const testFunc = () => {
-    return "Called test function"
+function testFunc() {
+    sendToWeb("Called test function");
 }
+
+function sendToWeb(data) {
+    window.postMessage(JSON.stringify(data));
+  }
