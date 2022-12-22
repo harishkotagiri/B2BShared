badc152462d9c05fe1643e825ffac98ff0ae610f
harishkotagiri <harish.kotagiri@gmail.cm>
2022-12-22 17:30:29 +0530

    Update shared.js

--- shared.js
+++ shared.js
@@ -2,7 +2,7 @@ window.getFullName = (fname, lname) => {
     return `${fname} ${lname}`;
 }
 
-const calculateVals = (val1, val2) => {
+window.calculateVals = (val1, val2) => {
     return val1+val2;
 }
 
