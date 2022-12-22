3a2c4d0ec8e6bdde465111b010338f1139868a54
harishkotagiri <harish.kotagiri@gmail.cm>
2022-12-22 17:05:18 +0530

    Update shared.js

--- shared.js
+++ shared.js
@@ -1,7 +1,11 @@
-const getFullName = (fname, lname) => {
+window.getFullName = (fname, lname) => {
     return `${fname} ${lname}`;
 }
 
 const calculateVals = (val1, val2) => {
     return val1+val2;
+}
+
+window.testFunc = () => {
+    return "Called test function"
 }
